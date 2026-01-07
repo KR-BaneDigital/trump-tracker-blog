d5bc90c2b3b5a6afcdb7b85301cf99719b782fed
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-07 15:20:53 -0500

    Initialize repository from template (production) (batch 2/2)

---
+++ next.config.mjs
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pixabay.com',
+      },
+    ],
+  },
+}
+
+export default nextConfig
