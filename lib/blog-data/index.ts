a816cac03a433e5cf437566524bc0b43e9f85dcf
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-07 18:42:09 -0500

    Remove breaking-news-what-fox-news-trump-alert-means-for-viewers from blog-data index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -1,11 +1,9 @@
 // Blog Data Index - APPEND-ONLY System
 // 🤖 Backend appends to this file when new posts are built
 
-import { post as breaking_news_what_fox_news_trump_alert_means_for_viewers } from './breaking-news-what-fox-news-trump-alert-means-for-viewers.data'
 import { post as military_maneuvers_or_just_talk_analyzing_trump_s_greenland_ } from './military-maneuvers-or-just-talk-analyzing-trump-s-greenland-.data'
 
 // Export all posts (newest first)
 export const allPosts = [
   military_maneuvers_or_just_talk_analyzing_trump_s_greenland_,
-  breaking_news_what_fox_news_trump_alert_means_for_viewers
 ]
