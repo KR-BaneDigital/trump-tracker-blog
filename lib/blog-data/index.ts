f87afe4c480f6206128ebc66181e0785ac44e868
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-08 17:59:00 -0500

    Remove stay-ahead-of-the-curve-unpacking-trump-breaking-news-today from blog-data index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -9,11 +9,9 @@ import { post as swinging_with_the_stars_trump_s_florida_golf_partners_reveal } 
 import { post as kai_trump_s_wrist_surgery_the_comeback_story_you_don_t_want_ } from './kai-trump-s-wrist-surgery-the-comeback-story-you-don-t-want-.data'
 import { post as how_long_will_she_be_unable_to_play_golf_a_game_changing_upd } from './how-long-will-she-be-unable-to-play-golf-a-game-changing-upd.data'
 import { post as stay_ahead_of_the_curve_why_trump_breaking_news_is_a_game_ch } from './stay-ahead-of-the-curve-why-trump-breaking-news-is-a-game-ch.data'
-import { post as stay_ahead_of_the_curve_unpacking_trump_breaking_news_today } from './stay-ahead-of-the-curve-unpacking-trump-breaking-news-today.data'
 
 // Export all posts (newest first)
 export const allPosts = [
-  stay_ahead_of_the_curve_unpacking_trump_breaking_news_today,
   stay_ahead_of_the_curve_why_trump_breaking_news_is_a_game_ch,
   how_long_will_she_be_unable_to_play_golf_a_game_changing_upd,
   kai_trump_s_wrist_surgery_the_comeback_story_you_don_t_want_,
