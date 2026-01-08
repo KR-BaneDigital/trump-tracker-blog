7f8642de875cc72a870902b7721b662ad0c982dd
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-08 11:16:58 -0500

    [Auto-Publish] Index: swinging-with-the-stars-trump-s-florida-golf-partners-reveal

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -5,9 +5,11 @@ import { post as military_maneuvers_or_just_talk_analyzing_trump_s_greenland_ } 
 import { post as breaking_news_what_fox_news_trump_alert_means_for_viewers } from './breaking-news-what-fox-news-trump-alert-means-for-viewers.data'
 import { post as breaking_news_cnbc_s_alert_unveils_trump_s_major_setback } from './breaking-news-cnbc-s-alert-unveils-trump-s-major-setback.data'
 import { post as kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc } from './kai-trump-s-wrist-surgery-a-journey-from-injury-to-resilienc.data'
+import { post as swinging_with_the_stars_trump_s_florida_golf_partners_reveal } from './swinging-with-the-stars-trump-s-florida-golf-partners-reveal.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  swinging_with_the_stars_trump_s_florida_golf_partners_reveal,
   kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc,
   breaking_news_cnbc_s_alert_unveils_trump_s_major_setback,
   breaking_news_what_fox_news_trump_alert_means_for_viewers,
