fcfaa3dc60bb9a0a9c25ae72e855265abdc97a99
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-08 09:48:31 -0500

    [Auto-Publish] Index: kai-trump-s-wrist-surgery-a-journey-from-injury-to-resilienc

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -4,9 +4,11 @@
 import { post as military_maneuvers_or_just_talk_analyzing_trump_s_greenland_ } from './military-maneuvers-or-just-talk-analyzing-trump-s-greenland-.data'
 import { post as breaking_news_what_fox_news_trump_alert_means_for_viewers } from './breaking-news-what-fox-news-trump-alert-means-for-viewers.data'
 import { post as breaking_news_cnbc_s_alert_unveils_trump_s_major_setback } from './breaking-news-cnbc-s-alert-unveils-trump-s-major-setback.data'
+import { post as kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc } from './kai-trump-s-wrist-surgery-a-journey-from-injury-to-resilienc.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc,
   breaking_news_cnbc_s_alert_unveils_trump_s_major_setback,
   breaking_news_what_fox_news_trump_alert_means_for_viewers,
   military_maneuvers_or_just_talk_analyzing_trump_s_greenland_,
