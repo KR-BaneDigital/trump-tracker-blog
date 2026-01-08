c23691373dcd2523a3e52dc48a0adbbe010064d5
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-08 18:00:03 -0500

    Remove breaking-news-what-fox-news-trump-alert-means-for-viewers from blog-data index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -1,7 +1,6 @@
 // Blog Data Index - APPEND-ONLY System
 // 🤖 Backend appends to this file when new posts are built
 
-import { post as breaking_news_what_fox_news_trump_alert_means_for_viewers } from './breaking-news-what-fox-news-trump-alert-means-for-viewers.data'
 import { post as breaking_news_cnbc_s_alert_unveils_trump_s_major_setback } from './breaking-news-cnbc-s-alert-unveils-trump-s-major-setback.data'
 import { post as kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc } from './kai-trump-s-wrist-surgery-a-journey-from-injury-to-resilienc.data'
 import { post as swinging_with_the_stars_trump_s_florida_golf_partners_reveal } from './swinging-with-the-stars-trump-s-florida-golf-partners-reveal.data'
@@ -17,5 +16,4 @@ export const allPosts = [
   swinging_with_the_stars_trump_s_florida_golf_partners_reveal,
   kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc,
   breaking_news_cnbc_s_alert_unveils_trump_s_major_setback,
-  breaking_news_what_fox_news_trump_alert_means_for_viewers,
 ]
