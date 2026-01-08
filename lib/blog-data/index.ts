79852733b35d0719b46dfe925855d848d3e7e005
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-08 13:36:46 -0500

    [Auto-Publish] Index: how-long-will-she-be-unable-to-play-golf-a-game-changing-upd

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -7,9 +7,11 @@ import { post as breaking_news_cnbc_s_alert_unveils_trump_s_major_setback } from
 import { post as kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc } from './kai-trump-s-wrist-surgery-a-journey-from-injury-to-resilienc.data'
 import { post as swinging_with_the_stars_trump_s_florida_golf_partners_reveal } from './swinging-with-the-stars-trump-s-florida-golf-partners-reveal.data'
 import { post as kai_trump_s_wrist_surgery_the_comeback_story_you_don_t_want_ } from './kai-trump-s-wrist-surgery-the-comeback-story-you-don-t-want-.data'
+import { post as how_long_will_she_be_unable_to_play_golf_a_game_changing_upd } from './how-long-will-she-be-unable-to-play-golf-a-game-changing-upd.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  how_long_will_she_be_unable_to_play_golf_a_game_changing_upd,
   kai_trump_s_wrist_surgery_the_comeback_story_you_don_t_want_,
   swinging_with_the_stars_trump_s_florida_golf_partners_reveal,
   kai_trump_s_wrist_surgery_a_journey_from_injury_to_resilienc,
