95b9a799bee2edb66afcdd37fd507117bb7f9734
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-08 06:25:09 -0500

    Append breaking-news-cnbc-s-alert-unveils-trump-s-major-setback to blog index

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -3,9 +3,11 @@
 
 import { post as military_maneuvers_or_just_talk_analyzing_trump_s_greenland_ } from './military-maneuvers-or-just-talk-analyzing-trump-s-greenland-.data'
 import { post as breaking_news_what_fox_news_trump_alert_means_for_viewers } from './breaking-news-what-fox-news-trump-alert-means-for-viewers.data'
+import { post as breaking_news_cnbc_s_alert_unveils_trump_s_major_setback } from './breaking-news-cnbc-s-alert-unveils-trump-s-major-setback.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  breaking_news_cnbc_s_alert_unveils_trump_s_major_setback,
   breaking_news_what_fox_news_trump_alert_means_for_viewers,
   military_maneuvers_or_just_talk_analyzing_trump_s_greenland_,
 ]
