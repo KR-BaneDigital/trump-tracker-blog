ba628ff29d0b230d3a0ddbbd07aa1bb6680e1ebb
KR-BaneDigital <161838922+KR-BaneDigital@users.noreply.github.com>
2026-01-08 19:39:18 -0500

    [Auto-Publish] Index: waiting-for-snails-how-trump-s-doonbeg-resort-is-slowing-dow

--- lib/blog-data/index.ts
+++ lib/blog-data/index.ts
@@ -7,9 +7,11 @@ import { post as swinging_with_the_stars_trump_s_florida_golf_partners_reveal } 
 import { post as kai_trump_s_wrist_surgery_the_comeback_story_you_don_t_want_ } from './kai-trump-s-wrist-surgery-the-comeback-story-you-don-t-want-.data'
 import { post as how_long_will_she_be_unable_to_play_golf_a_game_changing_upd } from './how-long-will-she-be-unable-to-play-golf-a-game-changing-upd.data'
 import { post as how_a_rare_snail_could_snail_mail_trump_s_doonbeg_dreams } from './how-a-rare-snail-could-snail-mail-trump-s-doonbeg-dreams.data'
+import { post as waiting_for_snails_how_trump_s_doonbeg_resort_is_slowing_dow } from './waiting-for-snails-how-trump-s-doonbeg-resort-is-slowing-dow.data'
 
 // Export all posts (newest first)
 export const allPosts = [
+  waiting_for_snails_how_trump_s_doonbeg_resort_is_slowing_dow,
   how_a_rare_snail_could_snail_mail_trump_s_doonbeg_dreams,
   how_long_will_she_be_unable_to_play_golf_a_game_changing_upd,
   kai_trump_s_wrist_surgery_the_comeback_story_you_don_t_want_,
